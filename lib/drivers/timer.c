aa2338f3fe3f052553d6d70efe11da8250191d71
etienneluigi <etienne.luigi@gmail.com>
2017-08-29 22:44:34 +0200

    created timer files

---
+++ lib/drivers/timer.c
@@ -0,0 +1,6 @@
+/*
+ *Timer.c
+ *
+ *  Created on: 29/08/2017
+ *      Author: eluigi5685
+ */
---
+++ lib/drivers/timer.h
@@ -0,0 +1,22 @@
+/*
+ *Timer.h
+ *
+ *  Created on: 29/08/2017
+ *      Author: eluigi5685
+ */
+
+
+
+ // ------------------------
+ // Timers and Channels Used by default to be available to define differents servos frequency
+ // Example: 400hz for tail , 333hz for cyclic...
+ // ------------------------
+ /*
+    Timer | Channel 1 | Channel 2 | Channel 3 | Channel 4
+    ------+-----------+-----------+-----------+----------
+    TIM1  |  Servo 4  |           |           |
+    TIM2  |  Servo 6  |           |           |
+    TIM3  |  Servo 1  |  Servo 2  |  Servo3   |
+    TIM4  |  Servo 5  |           |           |
+    ------+-----------+-----------+-----------+----------
+*/
